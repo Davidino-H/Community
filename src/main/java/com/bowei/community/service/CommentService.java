ffd67f9ce19b3ea5a477f11f171ef3cde48861a4
DavidH <boweih@student.unimelb.edu.au>
2023-01-29 15:39:18 +1100

    Add comments and replies in the post

--- src/main/java/com/bowei/community/service/CommentService.java
+++ src/main/java/com/bowei/community/service/CommentService.java
@@ -2,20 +2,49 @@ package com.bowei.community.service;
 
 import com.bowei.community.dao.CommentMapper;
 import com.bowei.community.entity.Comment;
+import com.bowei.community.util.CommunityConstant;
+import com.bowei.community.util.OffensiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
     @Autowired
     private CommentMapper commentMapper;
+    @Autowired
+    private OffensiveFilter offensiveFilter;
+    @Autowired
+    private DiscussPostService discussPostService;
     public List<Comment> findCommentByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
     }
 
     public int findCommentCount(int entityType, int entityId) {
         return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+        if (comment == null) {
+            throw new IllegalArgumentException("Parameter is empty");
+        }
+        // add comments
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(offensiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // update post commentCount
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), count);
+        }
+
+        return rows;
     }
 }
