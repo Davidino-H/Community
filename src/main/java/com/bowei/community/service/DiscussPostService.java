e3814645c73eab34d02fdb8fdbefedbb38f1eb24
Davidino(Bowei_Huang) <boweih@student.unimelb.edu.au>
2023-03-15 15:55:48 +1100

    Implement a local cache for Server to optimize website performance. Tested by Jmeter, before optimization Throughout: 22/s. After optimization Throughout: 196/s.

--- src/main/java/com/bowei/community/service/DiscussPostService.java
+++ src/main/java/com/bowei/community/service/DiscussPostService.java
@@ -3,28 +3,99 @@ package com.bowei.community.service;
 import com.bowei.community.dao.DiscussPostMapper;
 import com.bowei.community.entity.DiscussPost;
 import com.bowei.community.util.OffensiveFilter;
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class DiscussPostService {
+    private static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
+
     @Autowired
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private OffensiveFilter offensiveFilter;
+    @Value("${caffeine.posts.max-size}")
+    private int maxSize;
+
+    @Value("${caffeine.posts.expire-seconds}")
+    private int expiredSeconds;
+
+    // Caffeine api: Cache, LoadingCache, AsyncLoadingCache
+
+    // Post list cache
+    private LoadingCache<String, List<DiscussPost>> postListCache;
+
+    // Post row cache
+    private LoadingCache<Integer, Integer> postRowCache;
+
+    @PostConstruct
+    public void init() {
+        // Initialize Post list cache
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expiredSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Override
+                    public @Nullable List<DiscussPost> load(@NonNull String key) throws Exception {
+                        if (key == null || key.length() == 0) {
+                            throw new IllegalArgumentException("Input error");
+                        }
+                        String[] params = key.split(":");
+                        if (params == null || params.length != 2) {
+                            throw new IllegalArgumentException("Input error");
+                        }
+                        int offset = Integer.valueOf(params[0]);
+                        int limit = Integer.valueOf(params[1]);
+
+                        logger.debug("load post list from DB.");
+                        return discussPostMapper.selectDiscussPosts(0, offset, limit, 1);
+                    }
+                });
+        // Initialize Post row cache
+        postRowCache= Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expiredSeconds,TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Override
+                    public @Nullable Integer load(@NonNull Integer integer) throws Exception {
+                        logger.debug("load post row from DB.");
+                        return discussPostMapper.selectDiscussPostRows(integer);
+                    }
+                });
+    }
+
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode) {
+        if (userId == 0 && orderMode == 1) {
+            return postListCache.get(offset + ":" + limit);
+        }
+        logger.debug("Load post list from DB.");
         return discussPostMapper.selectDiscussPosts(userId, offset, limit, orderMode);
     }
 
     public int findDiscussPostRows(int userId) {
+        if (userId == 0) {
+            return postRowCache.get(userId);
+        }
+        logger.debug("Load post row from DB.");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
     public int addDiscussPost(DiscussPost post) {
         if (post == null) {
-            throw new IllegalArgumentException("参数不能为空");
+            throw new IllegalArgumentException("Input can't be empty");
         }
 
         post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
