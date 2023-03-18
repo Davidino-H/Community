a5c90d60b95b75023d17571fbd416dd1d770bc79
Davidino(Bowei_Huang) <boweih@student.unimelb.edu.au>
2023-03-18 16:09:29 +1100

    Implement forget password function.

--- src/main/java/com/bowei/community/service/UserService.java
+++ src/main/java/com/bowei/community/service/UserService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -234,4 +236,63 @@ public class UserService implements CommunityConstant {
         });
         return list;
     }
+
+    //忘记密码之后给邮箱发送验证码
+    public Map<String, Object> getCode(String email) {
+        Map<String,Object> map = new HashMap<>();
+        //空值判断
+        if (StringUtils.isBlank(email)){
+            map.put("emailMsg","Please enter the email！");
+            return map;
+        }
+        //邮箱是否正确
+        User user = userMapper.selectByEmail(email);
+        if (user == null){
+            map.put("emailMsg","This email address has not been registered yet, please register before using it!");
+            return map;
+        }
+        //该用户还未激活
+        if (user.getStatus() == 0){
+            map.put("emailMsg","This mailbox is not yet activated, please go to the mailbox and activate it before using it!");
+            return map;
+        }
+        //邮箱正确的情况下，发送验证码到邮箱
+        Context context = new Context();
+        context.setVariable("email",email);
+        String code = CommunityUtil.generateUUID().substring(0,5);
+        context.setVariable("code",code);
+        String content = templateEngine.process("mail/forget", context);
+        mailClient.sendMail(email, "Community verification code", content);
+
+        map.put("code",code);//map中存放一份，为了之后和用户输入的验证码进行对比
+        map.put("expirationTime", LocalDateTime.now().plusMinutes(5L));//过期时间
+        return map;
+    }
+
+    //忘记密码
+    public Map<String, Object> forget(String email, String verifycode, String password, HttpSession session){
+        Map<String,Object> map = new HashMap<>();
+        //空值处理
+        if (StringUtils.isBlank(email)){
+            map.put("emailMsg","Please enter your email address！");
+            return map;
+        }
+        if (StringUtils.isBlank(verifycode)){
+            map.put("codeMsg","Please enter the verification code！");
+            return map;
+        }
+        if (StringUtils.isBlank(password)){
+            map.put("passwordMsg","Please enter a new password！");
+            return map;
+        }
+
+        //邮箱在获取验证码那一步已经验证过了，是有效的邮箱，且验证码也有效
+        User user = userMapper.selectByEmail(email);
+        password = CommunityUtil.md5(password + user.getSalt());
+        userMapper.updatePassword(user.getId(),password);
+
+        return map;
+    }
+
+
 }
