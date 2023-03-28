fe838b99c33401fbf3b9271e6f4fdbe4062a82be
Davidino(Bowei_Huang) <boweih@student.unimelb.edu.au>
2023-03-28 22:09:05 +1100

    Fix some bugs

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -6,7 +6,7 @@ window.alert = function(message) {
 				'<div class="modal-dialog" role="document">'+
 				'<div class="modal-content">'+
 					'<div class="modal-header">'+
-						'<h5 class="modal-title">提示</h5>'+
+						'<h5 class="modal-title">Tips</h5>'+
 						'<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
 							'<span aria-hidden="true">&times;</span>'+
 						'</button>'+
@@ -15,7 +15,7 @@ window.alert = function(message) {
 						'<p></p>'+
 					'</div>'+
 					'<div class="modal-footer">'+
-						'<button type="button" class="btn btn-secondary" data-dismiss="modal">确定</button>'+
+						'<button type="button" class="btn btn-secondary" data-dismiss="modal">Enter</button>'+
 					'</div>'+
 					'</div>'+
 				'</div>'+
